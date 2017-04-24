5caa82d0d1aaec130111e573e36eaf78265d3f6a
emery-dev <eandrewzap@gmail.com>
2017-04-24 19:25:50 -0400

    update build

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ var index = require('./routes/index');
 
 var app = express();
 
-app.use(express.static(path.join(__dirname + '/routes')));
+//app.use(express.static(path.join(__dirname + '/routes')));
+
+app.set('port', (process.env.PORT || 3001));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -15,4 +21,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', index);
 
-module.exports = app;
+app.get('./../api/todos', (req, res) => {
+  const param = req.query.q;
+
+  if (!param) {
+    res.json({
+      error: 'Missing required parameter "q"',
+
+    });
+    return;
+  }
+})
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+});
+
+//module.exports = app;
