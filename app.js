fbfba81720209aba6cfa790f2d1ba7ab3c1c77db
emery-dev <eandrewzap@gmail.com>
2017-04-24 21:28:32 -0400

    change table ref

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var index = require('./routes/index');
 
 var app = express();
 
-app.set('port', (process.env.PORT || 3001));
+app.set('port', (process.env.DATABASE_URL || 3001));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
@@ -23,7 +23,7 @@ app.use('/', index);
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
+    client.query('SELECT * FROM todo', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
