f227d262cd9e1b77dbbd4313e2159cb49b0f1976
emery-dev <eandrewzap@gmail.com>
2017-04-24 20:25:08 -0400

    add cors to app

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-
+var cors = require('cors');
 var index = require('./routes/index');
 
 var app = express();
+
 
 app.set('port', (process.env.PORT || 3001));
 
@@ -13,6 +14,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
