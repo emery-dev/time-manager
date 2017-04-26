b59a5106551daff6042ba7d4065c8291ff87bb76
emery-dev <eandrewzap@gmail.com>
2017-04-26 12:16:12 -0400

    build

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var pgp = require('pg-promise');
-//var cors = require('cors');
 var index = require('./routes/index');
 
 var app = express();
-
-const url = process.env.DATABASE_URL || `http://localhost:3001`;
 
 app.set('port', (process.env.PORT || 3001));
 
@@ -20,7 +17,6 @@ app.use(express.static('./build'));
 
 app.use(express.static(path.join(__dirname + '/public')));
 
-//app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,19 +27,8 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname + '/public'));
 });
 
-//pgp.defaults.ssl = true;
-// pgp.connect(url, function(err, client) {
-//   if (err) throw err;
-//   console.log('Connected to postgres! Getting schemas...');
-//
-//   client
-//     .query('SELECT * FROM todo;')
-//     .on('row', function(row) {
-//       console.log(JSON.stringify(row));
-//     });
-// });
-
 app.listen(app.get('port'), () => {
   console.log('server started on ' + app.get('port'));
+  console.log(process.env);
+  console.log(process.env.PORT);
 });
-//console.log('server started on ' ${app.get('port')});
--- src/utils/daily-manager-api.js
+++ src/utils/daily-manager-api.js
@@ -3,7 +3,8 @@ import { getIdToken } from './AuthService';
 
 var connectionString = '';
 
-const BASE_URL = process.env.PORT || 'http://localhost:3001';
+const BASE_URL = 'http://localhost:3001';
+const BASE_URL2 = process.env.PORT;
 
 export function getDailySchedule() {
   console.log(process.env.DATABASE_URL);
