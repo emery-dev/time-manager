bfccff8c9ba5b9621e89d55c92f05fd38a0839a1
emery-dev <eandrewzap@gmail.com>
2017-04-25 21:45:40 -0400

    build

--- src/utils/daily-manager-api.js
+++ src/utils/daily-manager-api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import { getIdToken } from './AuthService';
 
-const BASE_URL = process.env.DATABASE_NAME || 'http://localhost:3000';
+const BASE_URL = process.env.DATABASE_URL || 'http://localhost:3000';
 
 export function getDailySchedule() {
+  console.log(process.env.DATABASE_URL);
   const url = `${BASE_URL}/api/todos`;
   console.log('url: ' + url);
   return axios.get(url, { headers: { Authorization: `Bearer ${getIdToken()}` }}).then(response => response.data);
