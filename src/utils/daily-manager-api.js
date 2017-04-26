24b4bd2718cff5e7dcf219eba4fcac4263ad25b5
emery-dev <eandrewzap@gmail.com>
2017-04-26 12:21:57 -0400

    build

--- src/utils/daily-manager-api.js
+++ src/utils/daily-manager-api.js
@@ -3,13 +3,10 @@ import { getIdToken } from './AuthService';
 
 var connectionString = '';
 
-const BASE_URL = 'http://localhost:3001';
+const BASE_URL = 'http://time-manager-emery.herokuapp.com';
 const BASE_URL2 = process.env.PORT;
 
 export function getDailySchedule() {
-  console.log(process.env.DATABASE_URL);
-  console.log(process.env.PORT);
-
   const url = `${BASE_URL}/api/todos`;
   console.log('url: ' + url);
   return axios.get(url, { headers: { Authorization: `Bearer ${getIdToken()}` }}).then(response => response.data);
